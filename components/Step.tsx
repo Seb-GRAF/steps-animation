79242b6ebba2a52b0d6b72c1f7e71246385c11b7
MorePog <95282692+MorePog@users.noreply.github.com>
2022-09-21 22:07:02 +0200

    first commit

---
+++ components/Step.tsx
@@ -0,0 +1,71 @@
+import { motion } from 'framer-motion'
+import CheckIcon from './CheckIcon'
+
+interface Props {
+  step: number
+  currentStep: number
+}
+
+const Step = ({ step, currentStep }: Props): JSX.Element => {
+  let status =
+    currentStep === step
+      ? 'active'
+      : currentStep < step
+      ? 'inactive'
+      : 'complete'
+
+  return (
+    <motion.div className='relative' animate={status}>
+      <motion.div
+        variants={{
+          active: {
+            scale: 1,
+            transition: {
+              delay: 0,
+              duration: 0.2,
+            },
+          },
+          complete: {
+            scale: 1.25,
+            transition: {
+              duration: 0.6,
+              delay: 0.1,
+              type: 'tween',
+              ease: 'circOut',
+            },
+          },
+        }}
+        className='absolute inset-0 bg-blue-200 rounded-full'></motion.div>
+      <motion.div
+        initial={false}
+        variants={{
+          inactive: {
+            backgroundColor: '#fff',
+            borderColor: '#e2e8f0',
+            color: '#94a3b8',
+          },
+          active: {
+            backgroundColor: '#fff',
+            borderColor: '#3b82f6',
+            color: '#3b82f6',
+          },
+          complete: {
+            backgroundColor: '#3b82f6',
+            borderColor: '#3b82f6',
+            color: '#3b82f6',
+          },
+        }}
+        transition={{ duration: 0.2 }}
+        className='relative flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold'>
+        <div className='flex items-center justify-center'>
+          {status === 'complete' ? (
+            <CheckIcon className='h-6 w-6 text-white' />
+          ) : (
+            <span>{step}</span>
+          )}
+        </div>
+      </motion.div>
+    </motion.div>
+  )
+}
+export default Step
